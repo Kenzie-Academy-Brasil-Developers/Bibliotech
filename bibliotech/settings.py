edca64b78bd39391d159e831488a99c50468d107
antonio <antoniomarcosnogueiraalmeida@gmail.com>
2022-07-15 16:45:19 -0300

    feat: docker re-enabled

--- bibliotech/settings.py
+++ bibliotech/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-#from dotenv import load_dotenv
-#from os import getenv
+from dotenv import load_dotenv
+from os import getenv
 
-#load_dotenv()
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wvdu&65q(3go=o_&jt_r_9xse5aewnx%dwq3l2(=f*q0le!@go'
-#SECRET_KEY = getenv('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-wvdu&65q(3go=o_&jt_r_9xse5aewnx%dwq3l2(=f*q0le!@go'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -92,18 +92,14 @@ WSGI_APPLICATION = 'bibliotech.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
-
-
-
-"""
 print(getenv("POSTGRES_DB"))
 
 DATABASES = {
@@ -117,7 +113,7 @@ DATABASES = {
         "PORT": 5432
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
