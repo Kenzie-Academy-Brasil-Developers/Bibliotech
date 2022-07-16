6f02b40cdb1db1bd38b39216d49b8ad99fc112cc
Rodrigo Tavares <rodrigotvrs70@hotmail.com>
2022-07-15 21:08:24 -0300

    fix: removed http from allowed hosts

--- bibliotech/settings.py
+++ bibliotech/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://m5-capstone.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['m5-capstone.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
