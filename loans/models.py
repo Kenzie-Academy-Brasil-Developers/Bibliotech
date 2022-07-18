dcaee50063cc1c27422b60777c07f5573e7ebf2e
Junior da Silva <jr.a.silva@outlook.com>
2022-07-18 13:49:49 -0300

    build: tempo de devolução (30 dias)

--- loans/models.py
+++ loans/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from datetime import datetime, timedelta
 
+def return_date():
+    return datetime.now() + timedelta(days=30)
+
 class Loan(models.Model):
     loan_date = models.DateField(auto_now_add=True)
-    return_date = models.DateField(loan_date + timedelta(days=30))
+    return_date = models.DateField(default=return_date)
     is_returned = models.BooleanField(default=False) 
     
     user = models.ForeignKey(
