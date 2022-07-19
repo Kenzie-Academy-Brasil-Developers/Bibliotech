35a35b3dc936a6bd43ae2705910836386f629ca7
Junior da Silva <jr.a.silva@outlook.com>
2022-07-19 12:18:43 -0300

    build: get loan

--- loans/urls.py
+++ loans/urls.py
@@ -3,5 +3,6 @@ from django import views
 from . import views
 
 urlpatterns = [
-    path('loan/', views.ListLoans.as_view())
+    path('loan/', views.ListLoansView.as_view()),
+    path('loan/<int:pk>/', views.LoanDetailView.as_view())
 ]
--- loans/views.py
+++ loans/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from loans.models import Loan
+from loans.serializers import LoanSerializer
+    
+class ListLoansView(generics.ListCreateAPIView):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer      
+
+class LoanDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Loan.objects.all()
+    serializer_class = LoanSerializer
