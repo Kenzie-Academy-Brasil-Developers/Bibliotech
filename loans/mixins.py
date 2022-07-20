64e50f09a5d2a1906c813949612d2b76d57bcd84
Tiago <m.tiago@outlook.com.br>
2022-07-20 18:49:03 -0300

    fix: Loan

--- loans/mixins.py
+++ loans/mixins.py
@@ -1,3 +1,3 @@
 class SerializerByMethodMixin:
     def get_serializer_class(self, *args, **kwargs):
-        return self.serializer_map.get(self.request.method, self.serializer_class)
+        return self.serializer_map.get(self.request.method, self.serializer_class) docker build . -t casptone-m5-g12
