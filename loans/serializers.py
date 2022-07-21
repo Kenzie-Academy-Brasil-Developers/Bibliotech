9071970c16ef8aa39604cc77bc6b065beb4d8fe9
Tiago <m.tiago@outlook.com.br>
2022-07-21 00:41:13 -0300

    fix: views, serializer

--- loans/serializers.py
+++ loans/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from books.models import Book
 from books.serializers import BookSerializer
 
+from users.models import User
+
 from loans.models import Loan
 from users.serializers import UserSerializer, UserLoanSerializer
 
@@ -24,9 +26,34 @@ class CreateLoanSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         book_data = validated_data.pop("book_id")
+        user_data = validated_data["user"]
         book = Book.objects.get(id=book_data)
-        loan = Loan.objects.create(**validated_data, book_id=book)      
+        loan = Loan.objects.create(**validated_data, book_id=book)  
+        User.objects.filter(email=user_data).update(is_debt = "True")
+        Book.objects.filter(id=book_data).update(is_available = "False")
+
         return loan    
+
+class CreateReturnSerializer(serializers.ModelSerializer):
+
+    user = UserLoanSerializer(read_only=True)
+    
+    class Meta:
+        model = Loan
+        fields = "__all__"
+        read_only_fields = ["id", "loan_date", "return_date", "is_returned", "user", "book_id"]
+
+    def update(self, instance, validated_data):
+        
+        loan_data = book_data = instance.__dict__["id"]
+        book_data = instance.book_id.__dict__["id"]
+        user_data = instance.user
+        User.objects.filter(email=user_data).update(is_debt = "False")
+        Book.objects.filter(id=book_data).update(is_available = "True")
+        loan = Loan.objects.filter(id=loan_data).update(is_returned = "True")
+
+        return loan
+
 
 
 class ListLoanSerializer(serializers.ModelSerializer):
@@ -36,9 +63,3 @@ class ListLoanSerializer(serializers.ModelSerializer):
         model = Loan
         fields = "__all__"
 
-    def create(self, validated_data):
-        print(validated_data)
-        book_data = validated_data.pop("book_id")
-        book = Book.objects.get(id=book_data)
-        loan = Loan.objects.create(**validated_data, book_id=book)      
-        return loan  
